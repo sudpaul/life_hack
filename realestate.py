a5f631159b5509b65fc3233bd9f3deb4c89ae719
Sudipta Paul <sudipta116@gmail.com>
2018-12-17 13:14:50 +1100

    Develop get_data function to scrape real estate website

--- realestate.py
+++ realestate.py
@@ -3,49 +3,76 @@
 Created on Sun Dec  9 19:17:48 2018
 
 @author: Sudipta
+
 """
-headers = ({'User-Agent':
-            'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'})
-base_url = 'https://www.realestate.com.au/buy/property-house-in-sydney,+nsw+2000/list-1'
-url = 'https://www.realestate.com.au/buy/property-house-in-kellyville,+nsw+2155/list-1'
-parametes  
 
-from requests import get
+def get_data(location, post_code):
+    
+    from requests import get
+    from bs4 import BeautifulSoup
+    import pandas as pd
+    from time import sleep
+    from random import randint
+    from collections import defaultdict
+    
+    data  = defaultdict(list)
+    #headers = ({'User-Agent':'Mozilla/5.0 (Windows NT 6.1) 
+    #                         AppleWebKit/537.36 (KHTML, like Gecko)
+    #                         Chrome/70.0.3538.110 Safari/537.36'})
+    url = 'https://www.realestate.com.au/buy/property-house-in-{loc},+nsw+{code}/list-'.format(loc=location, code=post_code)
+    
+    n_pages = 0
 
-response = get(base_url)
+    for page in range(0,40):
+        n_pages += 1
+        response = get(url+str(n_pages))
+        html_soup = BeautifulSoup(response.text, 'html.parser')
 
-from bs4 import BeautifulSoup
+        house_containers = html_soup.find_all('div', class_="residential-card__content")
+        if house_containers != []:       
+           for house in house_containers:   
+               try:
+                   data["price"].append(house.find_all('span', class_='property-price')[0].text)
+               except:
+                   data["price"].append(None)
+            
+               try:
+                   data["location"].append(house.find_all('span', class_='')[1].text)
+               except:
+                   data["location"].append(None)
+             
+               try:
+                   data["property_type"].append(house.find_all('span', class_="residential-card__property-type")[0].text)
+               except:
+                   data["property_type"].append(None)
+             
+               try:
+                   data["beds"].append(house.find_all('span', class_="general-features__icon general-features__beds")[0].text)
+               except:
+                   data["beds"].append(None)
+               try:
+                   data["baths"].append(house.find_all('span', class_="general-features__icon general-features__baths")[0].text)
+               except:
+                   data["baths"].append(None)
+            
+               try:
+                   data["parking"].append(house.find_all('span', class_="general-features__icon general-features__cars")[0].text)
+               except:
+                   data["parking"].append(None)
+                            
+               try:
+                   data["size_m2"].append(house.find_all('span', class_="property-size__icon property-size__land")[0].text)   
+               except:
+                   data["size_m2"].append(None)
+        else:
+              break
+    
+        sleep(randint(1,2))       
 
-html_soup = BeautifulSoup(response.text, 'html.parser')
+    df = pd.DataFrame.from_dict(data)
+ 
+    print('You scraped {} pages containing {} properties.'.format(n_pages, df.shape[0]))
+    
+    return df
+   
 
-house_containers = soup.find_all('div', class_="residential-card__content-wrapper")
- 
-first =house_containers[0]
- 
-first.find_all('span')
- 
- <--------------> output
- 
- [<span class="property-price ">Auction - $1,475,000</span>,
- <span class="">28 Kingfield Road, Kellyville</span>,
- <span aria-label="House property type" class="residential-card__property-type" role="text">House</span>,
- <span class="residential-card__with-comma"></span>,
- <span class="general-features__icon general-features__beds"> <!-- -->6</span>,
- <span class="general-features__icon general-features__baths"> <!-- -->6</span>,
- <span class="general-features__icon general-features__cars"> <!-- -->2</span>,
- <span aria-hidden="true" class="property-size__icon property-size__land"> <!-- -->550</span>,
- <span aria-hidden="true"> <!-- -->m²</span>,
- <span aria-hidden="true" class="agent__photo"><img alt="Mark Tasovac" class="agent__photo-image" src="https://i2.au.reastatic.net/120x160/53b9bfdc40bbd0af6b1730c2c2526b964546fb172a179d3b2aa59fef8b228539/main.jpg"/></span>,
- <span class="save_icon__hollow"></span>,
- <span class="save_icon__filled"></span>,
- <span class="listing-bookmark__text">Save</span>,
- <span class="residential-card__details-button-text">Details</span>,
- <span class="residential-card__details-button-icon"></span>]
- 
-price = first.find_all('span', class_='property-price')[0].text
-location = first.find_all('span', class_='')[1].text
-property_type = first.find_all('span', class_="residential-card__property-type")[0].text
-beds = first.find_all('span', class_="general-features__icon general-features__beds")[0].text
-baths = first.find_all('span', class_="general-features__icon general-features__baths")[0].text
-car_parking = first.find_all('span', class_="general-features__icon general-features__cars")[0].text
-property_size = first.find_all('span', class_="property-size__icon property-size__land")[0].text                          

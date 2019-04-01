79d38039bb70d2ac5f9e3ffbc1a642b26af70ab7
Sudipta Paul <sudipta116@gmail.com>
2019-04-01 15:21:20 +1100

    Update the frontend app with backend script

--- desktop_app.py
+++ desktop_app.py
@@ -4,11 +4,55 @@ Created on Wed Feb 27 12:50:38 2019
 
 @author: z3525552
 """
-
+import backend
 from tkinter import *
-window=Tk()
 
+#Selection the data from GUI
+def get_selected_row(event):
+    
+    global selected_tuple
+    index =list1.curselection()[0] 
+    selected_tuple = list1.get(index)  
+    e1.delete(0,END)
+    e1.insert(END,selected_tuple[1])
+    e2.delete(0,END)
+    e2.insert(END,selected_tuple[2])
+    e3.delete(0,END)
+    e3.insert(END,selected_tuple[3])
+    e4.delete(0,END)
+    e4.insert(END,selected_tuple[4])
+
+#View All data from the database
+
+def view_command():
+    list1.delete(0,END)
+    for row in backend.view():
+        list1.insert(END,row)
+
+#Search data from the list box
+
+def search_command():
+    list1.delete(0,END)
+    for row in backend.search(unsw_id.get(),author_last.get(),author_first.get(),scopus_id.get()):
+        list1.insert(END,row)
+
+#Insert data to the database
+
+def add_command():
+    backend.insert(unsw_id.get(),author_last.get(),author_first.get(),scopus_id.get())        
+    list1.delete(0,END)
+    list1.insert(END,(unsw_id.get(),author_last.get(),author_first.get(),scopus_id.get()))
+
+# Delete the data from the database
+    
+def delete_command():
+    backend.delete(selected_tuple[1])
+
+
+window=Tk()
+#Title of the window object
 window.wm_title("Researcher")
+
 
 l1=Label(window,text="Zid")
 l1.grid(row=0,column=0)
@@ -47,22 +91,24 @@ sb1.grid(row=2,column=2,rowspan=12)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
-b1=Button(window,text="View all", width=12)
-b1.grid(row=2,column=3)
+list1.bind('<<ListboxSelect>>', get_selected_row)
 
-b2=Button(window,text="Search", width=12)
-b2.grid(row=3,column=3)
+b1=Button(window,text="View all", width=12,command=view_command)
+b1.grid(row=2,column=4)
 
-b3=Button(window,text="Add Researcher", width=16)
-b3.grid(row=4,column=3)
+b2=Button(window,text="Search", width=12, command=search_command)
+b2.grid(row=3,column=4)
+
+b3=Button(window,text="Add Researcher", width=16, command=add_command)
+b3.grid(row=4,column=4)
 
 b4=Button(window,text="Update Researcher", width=16)
-b4.grid(row=5,column=3)
+b4.grid(row=5,column=4)
 
-b5=Button(window,text="Delete Researcher", width=16)
-b5.grid(row=6,column=3)
+b5=Button(window,text="Delete Researcher", width=16, command=delete_command)
+b5.grid(row=6,column=4)
 
 b6=Button(window,text="Close", width=12)
-b6.grid(row=7,column=3)
+b6.grid(row=7,column=4)
 
 window.mainloop()

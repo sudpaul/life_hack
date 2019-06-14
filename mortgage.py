ec886c07c480d1ea28b6e0caacc6a8a1149efa3e
Sudipta Paul <sudipta116@gmail.com>
2019-06-14 15:51:15 +1000

    Update the compare Mortgage function

--- mortgage.py
+++ mortgage.py
@@ -69,15 +69,100 @@ class TwoRate(Mortgage):
 
 def compareMortgages(amount, years, fixedRate, pts, ptsRate, varRate1,
                      varRate2, varMonths):
+    '''Benchmarking different mortgages 
+    
+    amount = loan amount
+    years = total years of repayment
+    fixedRate = fixed interest rate for repayment --- example 0.07 is 7%
+    pts = interest rate changes 3.25
+    ptsRate = Fixed and variable rate for repayment
+    varRate1 = intial varibale rate for repayment
+    varRate2  = After rate hikes variable rate
+    varMonths = Number of months for varibale rate
+    '''        
     total_months = years*12
     fixed1 = Fixed(amount, fixedRate, total_months)
     fixed2 = FixedandVariable(amount, ptsRate, total_months, pts)
     twoRate = TwoRate(amount, varRate2, total_months, varRate1, varMonths)
     
     mortgages = [fixed1, fixed2, twoRate]
-    for month in range(total_months):
-        for mortgage in mortgages:
-            mortgage.makePayment()
-    for m in mortgages:
-        print(m)
-        print(' Total payments = $' + str(int(m.getTotalPaid())))
+    for m in range(total_months):
+        for mort in mortgages:
+            mort.makePayment()
+    plotMortgages(mortgages, amount)
+        
+# Visulisation 
+import numpy as np
+import matplotlib.pyplot as plt
+        
+def plotPayments(self, style):
+    
+    plt.plot(self.paid[1:], style, label = self.legend)
+
+def plotBalance(self, style):
+    
+    plt.plot(self.owed, style, label = self.legend)
+
+
+def plotTotPd(self, style):
+    """Plot the cumulative total of the payments made"""
+    totPd = [self.paid[0]]
+    for i in range(1, len(self.paid)):
+        totPd.append(totPd[-1] + self.paid[i])
+    
+    plt.plot(totPd, style, label = self.legend)
+
+def plotNet(self, style):
+    """Plot an approximation to the total cost of the mortgage
+      over time by plotting the cash expended minus the equity
+      acquired by paying off part of the loan"""
+    totPd = [self.paid[0]]
+    for i in range(1, len(self.paid)):
+        totPd.append(totPd[-1] + self.paid[i])
+ #Equity acquired through payments is amount of original loan
+ # paid to date, which is amount of loan minus what is still owed
+    equityAcquired = np.array([self.loan]*len(self.owed))
+    equityAcquired = equityAcquired - np.array(self.owed)
+    net = np.array(totPd) - equityAcquired
+    plt.plot(net, style, label = self.legend)  
+    
+    
+def plotMortgages(morts, amt):
+    styles = ['b-', 'b-.', 'b:']
+ #Give names to figure numbers
+    payments = 0
+    cost = 1
+    balance = 2
+    netCost = 3
+    plt.figure(payments)
+    plt.title('Monthly Payments of Different $' + str(amt)
+     + ' Mortgages')
+    plt.xlabel('Months')
+    plt.ylabel('Monthly Payments')
+    plt.figure(cost)
+    plt.title('Cash Outlay of Different $' + str(amt) + ' Mortgages')
+    plt.xlabel('Months')
+    plt.ylabel('Total Payments')
+    plt.figure(balance)
+    plt.title('Balance Remaining of $' + str(amt) + ' Mortgages')
+    plt.xlabel('Months')
+    plt.ylabel('Remaining Loan Balance of $')
+    plt.figure(netCost)
+    plt.title('Net Cost of $' + str(amt) + ' Mortgages')
+    plt.xlabel('Months')
+    plt.ylabel('Payments - Equity $')
+    for i in range(len(morts)):
+         plt.figure(payments)
+         morts[i].plotPayments(styles[i])
+         plt.figure(cost)
+         morts[i].plotTotPd(styles[i])
+         plt.figure(balance)
+         morts[i].plotBalance(styles[i])
+         plt.figure(netCost)
+         morts[i].plotNet(styles[i])
+    plt.figure(payments)
+    plt.legend(loc = 'upper center')
+    plt.figure(cost)
+    plt.legend(loc = 'best')
+    plt.figure(balance)
+    plt.legend(loc = 'best')

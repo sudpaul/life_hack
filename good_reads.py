023c3049cbd85df76c0b759dc1fb9108b0c72a7a
Sudipta Paul <sudipta116@gmail.com>
2019-11-08 11:07:45 +1100

    Testing good reads api

---
+++ good_reads.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  6 11:26:57 2018
+
+@author: z3525552
+"""
+import requests
+
+def api_key(file_name):
+     
+    with open(file_name) as f:
+        key = f.read().strip()
+    return key
+
+def api_query(api_file): 
+     
+    key = api_key(api_file)
+    base_url ="https://www.goodreads.com/book/title.json"
+    query = {"key": key, 'format':'json',"title": "Outliers"}
+    try:
+        response = requests.get(url=base_url, params=query)
+        if not response.status_code == 200:
+            return response.status_code
+        else:
+       
+            response_data = response.json()       
+            return response_data
+    except:
+          return 0
